851fc0db3956dfb54d389def80cce1aff5e49ae7
Jack Power <20080169@wit.ie>
2019-11-08 11:50:25 +0000

    Added item filtering functionality

--- media-manager/src/App.js
+++ media-manager/src/App.js
@@ -10,9 +10,11 @@ import request from "superagent";
 class App extends Component {
 
   state = {
-    selectedItems: []
+    selectedItems: [],
+    searchText: '',
+    mediaType: 'all',
+    mediaStatus: 'in progress'
   };
-
 
   componentDidMount() {
     console.log("componentDidMount of App");
@@ -27,7 +29,36 @@ class App extends Component {
     });
   }
 
-  updateSelected (id) {
+  statusMap(statusName) { //map status name to database status value
+    switch (statusName){
+      case 'queued':
+        return -1;
+      case 'in progress':
+        return 0;
+      case 'completed':
+        return 1;
+    }
+  }
+
+  updateFilter(event) {
+    this.setState({
+        [event.target.name]: event.target.value.toLowerCase()
+      });
+  }
+
+  getItems = () => {
+    let titleMatch = new RegExp('\\b'+this.state.searchText); //match only from start of words
+    return localCache.getAll()
+      .filter(
+        item => (
+          (item.title.toLowerCase().search(titleMatch) !== -1) &&
+          (this.state.mediaType === 'all' || this.state.mediaType === item.type) &&
+          (this.statusMap(this.state.mediaStatus) === item.itemStatus)
+        )
+      )
+  }
+
+  updateSelected(id) {
     let items = this.state.selectedItems;
     let index = items.indexOf(id);
     if (index >= 0){ //if already selected
@@ -46,9 +77,9 @@ class App extends Component {
       <Container fluid>
         <Toolbar />
         <Row>
-          <SearchBar />
+          <SearchBar text={this.state.searchText} mediaType={this.state.mediaType} mediaStatus={this.state.mediaStatus} handleChange={this.updateFilter.bind(this)}/>
           <Col>
-            <MainView items={localCache.getAll()} updateSelected={this.updateSelected.bind(this)} isSelected={(id) => this.state.selectedItems.includes(id)}/>
+            <MainView items={this.getItems()} updateSelected={this.updateSelected.bind(this)} isSelected={(id) => this.state.selectedItems.includes(id)}/>
           </Col>
         </Row>
       </Container>
--- media-manager/src/components/searchBar/index.js
+++ media-manager/src/components/searchBar/index.js
@@ -1,58 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
-class SearchBar extends Component {
+const SearchBar = (props) => (
 
-  constructor(props) {
-    super(props);
+  <Col xs={3} sm={4} md={3} lg={3} xl={2} className='vh-100 border pt-3 ml-3'>
+    <Form.Control type='text' name='searchText' placeholder='Search...' className='mb-4' onChange={props.handleChange}/>
 
-    this.state = {
-      text: '',
-      mediaType: 'all',
-      mediaStatus: 'in progress'
-    }
-
-    this.handleChange.bind(this);
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <Col xs={3} sm={4} md={3} lg={3} xl={2} className='vh-100 border pt-3 ml-3'>
-        <Form.Control type='text' name='text' placeholder='Search...' className='mb-4' onChange={this.handleChange}/>
- 
-        <Form.Group as={Row}>
-          <Form.Label column sm={3}>Type</Form.Label>
-          <Col sm={9}>
-            <Form.Control as='select' name='mediaType' value={this.state.mediaType} onChange={this.handleChange}>
-              <option value='all'>All</option>
-              <option value='movies'>Movies</option>
-              <option value='series'>Series</option>
-              <option value='books'>Books</option>
-              <option value='music'>Music</option>
-            </Form.Control>
-          </Col>
-        </Form.Group>
-
-        <fieldset>
-          <Form.Group as={Row}>
-            <Form.Label as='legend' column sm={3} className='pt-0'>Status</Form.Label>
-            <Col sm={9}>
-              <Form.Check id='statusRadio1' type='radio' name='mediaStatus' label='Queued' value='queued' checked={this.state.mediaStatus === 'queued'} onChange={this.handleChange}/>
-              <Form.Check id='statusRadio2' type='radio' name='mediaStatus' label='In Progress' value='in progress' checked={this.state.mediaStatus === 'in progress'} onChange={this.handleChange}/>
-              <Form.Check id='statusRadio3' type='radio' name='mediaStatus' label='Completed' value='completed' checked={this.state.mediaStatus === 'completed'} onChange={this.handleChange}/>
-            </Col>
-          </Form.Group>
-        </fieldset>
+    <Form.Group as={Row}>
+      <Form.Label column sm={3}>Type</Form.Label>
+      <Col sm={9}>
+        <Form.Control as='select' name='mediaType' value={props.mediaType} onChange={props.handleChange}>
+          <option value='all'>All</option>
+          <option value='movie'>Movies</option>
+          <option value='series'>Series</option>
+          <option value='book'>Books</option>
+          <option value='music'>Music</option>
+        </Form.Control>
       </Col>
-    );
-  }
-}
+    </Form.Group>
+
+    <fieldset>
+      <Form.Group as={Row}>
+        <Form.Label as='legend' column sm={3} className='pt-0'>Status</Form.Label>
+        <Col sm={9}>
+          <Form.Check id='statusRadio1' type='radio' name='mediaStatus' label='Queued' value='queued' checked={props.mediaStatus === 'queued'} onChange={props.handleChange}/>
+          <Form.Check id='statusRadio2' type='radio' name='mediaStatus' label='In Progress' value='in progress' checked={props.mediaStatus === 'in progress'} onChange={props.handleChange}/>
+          <Form.Check id='statusRadio3' type='radio' name='mediaStatus' label='Completed' value='completed' checked={props.mediaStatus === 'completed'} onChange={props.handleChange}/>
+        </Col>
+      </Form.Group>
+    </fieldset>
+  </Col>
+);
 
 export default SearchBar;
